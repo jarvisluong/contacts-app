8c8a901e2a5ce0d3f86c772b82c3f61ecbcd3e9a
Jarvis Luong <21980965+jarvisluong@users.noreply.github.com>
2022-12-21 09:48:30 +0700

    integrate expo-contacts

--- src/screens/ContactList.tsx
+++ src/screens/ContactList.tsx
@@ -1,4 +1,5 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import * as Contacts from "expo-contacts";
 import * as React from "react";
 import { Button, View, Text } from "react-native";
 import { RootStackNavigation } from "../router/routes";
@@ -6,10 +7,30 @@ import { RootStackNavigation } from "../router/routes";
 type Props = NativeStackScreenProps<RootStackNavigation>;
 
 export default function ContactList({ navigation }: Props) {
+  const [contactPermissionResponse, setContactPermissionResponse] =
+    React.useState<Contacts.PermissionStatus>();
+
+  React.useEffect(() => {
+    (async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      setContactPermissionResponse(status);
+    })();
+  }, []);
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-      <Button title="Go to Details" onPress={() => {}} />
+      {contactPermissionResponse === Contacts.PermissionStatus.DENIED && (
+        <Text>The app does not have access to the phone contacts</Text>
+      )}
+      {contactPermissionResponse === Contacts.PermissionStatus.UNDETERMINED && (
+        <Text>Awaiting the permission to access phone contacts</Text>
+      )}
+      {contactPermissionResponse === Contacts.PermissionStatus.GRANTED && (
+        <>
+          <Text>Home Screen</Text>
+          <Button title="Go to Details" onPress={() => {}} />
+        </>
+      )}
     </View>
   );
 }
